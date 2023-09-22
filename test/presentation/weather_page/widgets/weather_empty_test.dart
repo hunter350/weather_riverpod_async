6db06e35fb19548486ed512b702ead805d32e017
Andrey Popov <info@java4you.ru>
2023-09-22 21:58:39 +0300

    add test weather_empty_new.dart

--- test/presentation/weather_page/widgets/weather_empty_test.dart
+++ test/presentation/weather_page/widgets/weather_empty_test.dart
@@ -1,22 +1,25 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:riverpod_weather/weather/widgets/widgets.dart';
-
-
+import 'package:flutter_weather_bg_null_safety/bg/weather_bg.dart';
+import 'package:weather_riverpod_async/data/repository/model/models_repository.dart';
+import 'package:weather_riverpod_async/presentation/weather_page/widgets/weather_empty_new.dart';
 
 void main() {
+  final condition = WeatherCondition.clear;
   group('WeatherEmpty', () {
     testWidgets('renders correct text and icon', (tester) async {
       await tester.pumpWidget(
         MaterialApp(
           home: Scaffold(
-            body: WeatherEmpty(),
+            body: WeatherEmptyNew(weatherCondition: condition,),
           ),
         ),
       );
-      expect(find.text('Please Select a City!'), findsOneWidget);
-      expect(find.text('🏙️'), findsOneWidget);
+      await tester.pumpAndSettle();
+      // expect(find.text('Please Select a City!'), findsOneWidget);
+      // expect(find.text('🏙️'), findsOneWidget);
+      expect(find.byType(WeatherBg), findsOneWidget);
     });
   });
 }
