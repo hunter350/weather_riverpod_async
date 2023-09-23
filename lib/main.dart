94d4d24ccafdf7706cdcfecd83a9f627fd0b455d
Andrey Popov <info@java4you.ru>
2023-09-24 02:06:17 +0300

    move init SharedPref to shared_notifier.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'presentation/app.dart';
@@ -6,10 +7,7 @@ import 'state/shared_notifier.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final sharedPreferences = await SharedPreferences.getInstance();
-  if (sharedPreferences.getString('city') == null) {
-    sharedPreferences.setString('city', '');
-  }
+  SharedPreferences sharedPreferences = await initSharedPref();
   runApp(ProviderScope(overrides: [
     // override the previous value with the new object
     sharedPreferencesProvider.overrideWithValue(sharedPreferences),
--- lib/state/shared_notifier.dart
+++ lib/state/shared_notifier.dart
@@ -1,6 +1,15 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+Future<SharedPreferences> initSharedPref() async{
+  final sharedPreferences = await SharedPreferences.getInstance();
+  if (sharedPreferences.getString('city') == null) {
+    await sharedPreferences.setString('city', '');
+  }
+  return sharedPreferences;
+}
+
+
 final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
   throw UnimplementedError();
 });
