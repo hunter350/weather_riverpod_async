c07deca791a69531ca3d99a9f290b626f4e04b73
Andrey Popov <info@java4you.ru>
2023-10-10 04:18:43 +0300

    delete await in test shared_pref_init.dart

--- test/shared_pref_init.dart
+++ test/shared_pref_init.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<SharedPreferences> initShared() async {
-  await TestWidgetsFlutterBinding.ensureInitialized();
+  TestWidgetsFlutterBinding.ensureInitialized();
   SharedPreferences.setMockInitialValues({'city': ''});
   SharedPreferences sharedPref = await SharedPreferences.getInstance();
   return sharedPref;
